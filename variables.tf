06009913132639f75f4ef6ba9e96d109124d4fb2
krushal7 <94886146+krushal7@users.noreply.github.com>
2025-12-09 11:09:05 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "public_ips" {
+  description = "List of public IP addresses to share with other workspaces"
+  type        = list(string)
+
+  default = [
+    "8.8.8.8",
+    "1.1.1.1",
+  ]
+}
