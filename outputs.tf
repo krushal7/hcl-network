6cf08d3f4e3e37a014fa0038235b9c012099572d
krushal7 <94886146+krushal7@users.noreply.github.com>
2025-12-09 13:40:01 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,6 @@ output "public_ips" {
   description = "All public IP addresses defined in this workspace"
   value       = var.public_ips
 }
+output "force_trigger" {
+  value = var.force_trigger
+}
