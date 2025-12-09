fb1840709417c62a5099835d0d0c9a209b9e469b
krushal7 <94886146+krushal7@users.noreply.github.com>
2025-12-09 12:53:36 +0530

    Add environment output to outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "public_ips" {
   description = "All public IP addresses defined in this workspace"
   value       = var.public_ips
 }
+output "environment" {
+  description = "Environment name for this workspace"
+  value       = var.environment
+}
