cb5f9d3fc466a61ec0dbc111b9bd248e7195316f
krushal7 <94886146+krushal7@users.noreply.github.com>
2025-12-09 13:08:00 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,3 @@ output "public_ips" {
   description = "All public IP addresses defined in this workspace"
   value       = var.public_ips
 }
-output "environment" {
-  description = "Environment name for this workspace"
-  value       = var.environment
-}
